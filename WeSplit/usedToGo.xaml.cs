b81a2e9555b7808bb922c7daccca0858997f6910
kevinpro2000 <kevinpro2000@gmail.com>
2020-12-03 12:55:35 +0700

    đang đi và đã đi

--- WeSplit/usedToGo.xaml.cs
+++ WeSplit/usedToGo.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,6 +24,37 @@ namespace WeSplit
         public usedToGo()
         {
             InitializeComponent();
+        }
+        class Journey
+        {
+            public string Title { get; set; }
+            public string Image { get; set; }
+            public string Location { get; set; }
+        }
+
+        class JourneyDAO
+        {
+            public static BindingList<Journey> GetAll()
+            {
+                var result = new BindingList<Journey>()
+                {
+                    new Journey() { Title="Chu Tùng Nhân", Image="/Images/image1.jpg", Location="New York" },
+                    new Journey() { Title="Chu Tùng Nhân", Image="/Images/image2.jpg", Location="New York" },
+                    new Journey() { Title="Chu Tùng Nhân", Image="/Images/image3.jpg", Location="New York" },
+                    new Journey() { Title="Chu Tùng Nhân", Image="/Images/image4.jpg", Location="New York" },
+                    new Journey() { Title="Chu Tùng Nhân", Image="/Images/image5.jpg", Location="New York" },
+                    new Journey() { Title="Chu Tùng Nhân", Image="/Images/image6.jpg", Location="New York" },
+                };
+
+                return result;
+            }
+        }
+        BindingList<Journey> _list = new BindingList<Journey>();
+
+        private void UserControl_Loaded(object sender, RoutedEventArgs e)
+        {
+            _list = JourneyDAO.GetAll();
+            listJourney.ItemsSource = _list;
         }
     }
 }
