1c464b12bc7a9661d985f3900cce88dbee47463d
ngdkhoi <49549370+ngdkhoi@users.noreply.github.com>
2020-12-04 21:08:28 +0700

    Add DEMO SplashScreen

---
+++ WeSplit/SplashScreen.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Configuration;
+
+namespace WeSplit
+{
+    /// <summary>
+    /// Interaction logic for SplashScreen.xaml
+    /// </summary>
+    public partial class SplashScreen : Window
+    {
+        public SplashScreen()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Load(object sender, RoutedEventArgs e)
+        {
+            var value = ConfigurationManager.AppSettings["checkShowDialog"];
+            var showSplash = bool.Parse(value);
+
+            if (showSplash == false)
+            {
+                var screen = new MainWindow();
+                screen.Show();
+                this.Close();
+            }
+        }
+        private void turnOffButton_Click(object sender, RoutedEventArgs e)
+        {
+            var config = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+            config.AppSettings.Settings["checkShowDialog"].Value = "false";
+            config.Save(ConfigurationSaveMode.Minimal);
+        }
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow mw = new MainWindow();
+            mw.Show();
+            this.Close();
+        }
+    }
+}
---
+++ WeSplit/ViewModel/SplashScreenViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.ObjectModel;
+
+namespace WeSplit.ViewModel
+{
+    class SplashScreenViewModel : BaseViewModel
+    {
+        public string ShowImage { get => _ShowImage; set => _ShowImage = value; }
+
+        private string _ShowImage;
+        public SplashScreenViewModel()
+        {
+            ShowImage = "Images/image1.jpg";
+        }
+    }
+}
