411f32ad6ccccb62581d9d8b488b6b60b5c15ca3
Bach <doanxuanbach132@gmai.com>
2020-12-12 22:52:16 +0700

    done binding update database(not images)

--- WeSplit/Model/Member_ObjectPay_Cost.cs
+++ WeSplit/Model/Member_ObjectPay_Cost.cs
@@ -16,6 +16,15 @@ namespace WeSplit.Model
             set { _id = value; }
         }
 
+        private int _idPaid;
+
+        public int IdPaid
+        {
+            get { return _idPaid; }
+            set { _idPaid = value; }
+        }
+
+
         private string _name;
 
         public string Name
@@ -41,9 +50,10 @@ namespace WeSplit.Model
             set { _paid = value; }
         }
 
-        public Member_ObjectPay_Cost(int id, string name, string objectPaid, int? paid)
+        public Member_ObjectPay_Cost(int id, int idPaid, string name, string objectPaid, int? paid)
         {
             Id = id;
+            IdPaid = idPaid;
             Name = name;
             ObjectPaid = objectPaid;
             Paid = paid ?? default(int);
